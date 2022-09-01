e8184891ecec95626d1e02e68fb4312b411c8f36
PostRex <post-rex@pm.me>
2022-09-01 08:35:03 +0200

    imports can not be root bound

--- w_parse/src/item/import.rs
+++ w_parse/src/item/import.rs
@@ -2,7 +2,7 @@ use crate::expr::parse_many0;
 use crate::expr::path::{parse_path, ExprPath};
 use crate::{tag, ParResult, TokenSpan, Weak};
 use nom::branch::alt;
-use nom::combinator::{all_consuming, map};
+use nom::combinator::{all_consuming, map, verify};
 use nom::sequence::tuple;
 use nom::Parser;
 use w_tokenize::Kind;
@@ -32,7 +32,10 @@ fn parse_imports(i: TokenSpan) -> ParResult<Vec<Imports>> {
     let block = TokenSpan::new(i.file, block);
 
     let (_, imports) = all_consuming(parse_many0(alt((
-        map(parse_path, Imports::Single),
+        map(
+            verify(parse_path, |path| path.root.is_none()),
+            Imports::Single,
+        ),
         map(
             tuple((parse_path, Weak(Kind::Colon), parse_imports)),
             |(base, _, leaves)| Imports::Multiple(base, leaves),
